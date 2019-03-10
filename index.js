199b8ed5ee01d26202a3fef626845abb66e30546
Ronak Parekh <rparekh1996@gmail.com>
2019-03-10 23:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-    return "FUNKY!"
+  return function() {
+    
+  }
+
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
